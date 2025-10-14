edd8fac01f94a5ca98a96c6d2472ff9f30b01538
mareuy-compras <mareuystore@gmail.com>
2025-10-14 00:34:21 -0300

    Actualizo productos al 14-10-25

--- dist/sw.js
+++ dist/sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker para MARÉ Catálogo PWA
 // Versión del cache - incrementar cuando haya cambios importantes
-const CACHE_VERSION = 'mare-v1.2.5-auto-1760213642992';
+const CACHE_VERSION = 'mare-v1.2.5-auto-1760412848741';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const IMAGES_CACHE = `${CACHE_VERSION}-images`;
