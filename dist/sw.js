b30cbf05273bebb6a3cad658f5f308b713a094e8
fnosieski <fnosieski@gmail.com>
2025-12-08 20:09:05 -0300

    Actualizo productos al 08-12-25

--- dist/sw.js
+++ dist/sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker para MARÉ Catálogo PWA
 // Versión del cache - incrementar cuando haya cambios importantes
-const CACHE_VERSION = 'mare-v1.2.5-auto-1765142262107';
+const CACHE_VERSION = 'mare-v1.2.5-auto-1765235329892';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const IMAGES_CACHE = `${CACHE_VERSION}-images`;
